d88210995ce52b10c69278f4affa8bfd2a393c54
Jayant Shelke <jayantshelke4829@gmail.com>
2024-07-22 20:39:55 +0530

    Currency Converter

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import CurrencyConvertor from "./components/currency-convertor";
+
+function App() {
+  return (
+    <div className="min-h-screen  flex flex-col items-center justify-center" >
+    
+      <div className="container">
+        <CurrencyConvertor />
+      </div>
+    </div>
+  );
+}
+
+export default App;
